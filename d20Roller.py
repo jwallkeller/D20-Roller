748b1ae4a9a853a32b84e0bbbede902cdf7f82cf
Jack Keller <jwallkeller@gmail.com>
2024-03-21 10:54:06 -0400

    Add py file

---
+++ d20Roller.py
@@ -0,0 +1,17 @@
+
+### Use this space to try out ideas and free code ###
+
+import random
+
+d20 = int(input("How many d20s?"))
+
+sum = 0
+
+for x in range(1, d20):
+  roll = random.randint(1, 20)
+  sum += roll
+  
+avg = sum / d20
+  
+print("The sum of your roll(s): " + str(sum))
+print "The average of your roll(s): %.1f" % avg
