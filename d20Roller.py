80fd9ce7cb37610f213e3c7d1016055eff9021ca
Jack Keller <jwallkeller@gmail.com>
2026-05-29 09:38:51 -0400

    Remove print for sum. Update roll print statement

--- d20Roller.py
+++ d20Roller.py
@@ -9,11 +9,8 @@ sum = 0
 
 for x in range(0, d20):
   roll = random.randint(1, 20)
-  print("Roll " + str(x + 1) + ": " + str(roll))
+  print("Roll #" + str(x + 1) + ": " + str(roll))
   sum += roll
-  print("Running sum: " + str(sum) + "\n")
   
 avg = sum / d20
-  
-print("The sum of your roll(s): " + str(sum))
 print("The average of your roll(s): %.1f" % avg)
